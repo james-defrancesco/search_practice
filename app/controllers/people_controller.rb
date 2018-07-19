8e834ffac861d870ba66dc08d8801c7cbf5d28b5
t0nylombardi <alombardi.331@gmail.com>
2018-07-19 15:27:51 -0400

    add search feature

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,21 +1,24 @@
 class PeopleController < ApplicationController
+  helper_method :sort_column, :sort_direction
 
-    helper_method :sort_column, :sort_direction
-      def index
-        @search = Person.ransack(params[:q])
-        @search_results = @search.result
-        @people = Person.order(sort_column + ' ' + sort_direction)
-      end
+  def index
+    @q = Person.ransack(params[:q])
+    if params[:q]
+      @people = @q.result
+    else
+      @people = nil
+    end
+  end
 
-      def sort_column
-        if Person.column_names.include?(params[:sort])
-          return params[:sort]
-        else
-          return Person.column_names.first
-        end
-      end
+  def sort_column
+    if Person.column_names.include?(params[:sort])
+      params[:sort]
+    else
+      Person.column_names.first
+    end
+  end
 
-      def sort_direction
-        %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
-      end
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
+  end
 end
